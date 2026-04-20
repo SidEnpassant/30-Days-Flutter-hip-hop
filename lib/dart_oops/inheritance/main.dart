97331de99810a158dc3187bba367b9ef72089d93
Siddhes Das <siddhesdas2020@gmail.com>
2026-04-20 18:11:47 +0530

    inheritance it is

--- lib/dart_oops/inheritance/main.dart
+++ lib/dart_oops/inheritance/main.dart
@@ -1,37 +1,67 @@
+// void main(){
+//   Student student = Student();
+//
+//   student.name = 'Siddhes';
+//   student.age = 22;
+//   student.sem = '7th';
+//   student.regNumber = '232321312313134325';
+//
+//   student.displayInfo();
+//   student.displaySchoolInfo();
+// }
+//
+//
+//
+// // parent class
+// class Person {
+//   String? name;
+//   int? age;
+//
+//   void displayInfo(){
+//     print('Name: $name , Age: $age');
+//   }
+// }
+//
+//
+//
+// // child class or derived class
+// class Student extends Person{
+//   String? regNumber;
+//   String? sem;
+//
+//   void displaySchoolInfo(){
+//     print('Registration no : $regNumber');
+//     print('Semester : $sem');
+//   }
+//
+// }
+
+
 void main(){
-  Student student = Student();
+  Macbook macbook = Macbook('Macbook' , 154440 , 'Macbook Air');
 
-  student.name = 'Siddhes';
-  student.age = 22;
-  student.sem = '7th';
-  student.regNumber = '232321312313134325';
-
-  student.displayInfo();
-  student.displaySchoolInfo();
+  macbook.displayInfo();
 }
+// parent or base class
+class Laptop{
+  String brand;
+  double price;
 
-
-
-// parent class
-class Person {
-  String? name;
-  int? age;
+  // Constructor with parameters
+  Laptop(this.brand , this.price){
+    print('Laptop call');
+  }
 
   void displayInfo(){
-    print('Name: $name , Age: $age');
+    print('Brand: $brand , Price: $price');
   }
 }
 
-
-
-// child class or derived class
-class Student extends Person{
-  String? regNumber;
-  String? sem;
-
-  void displaySchoolInfo(){
-    print('Registration no : $regNumber');
-    print('Semester : $sem');
+class Macbook extends Laptop{
+  String model;
+  Macbook(String brand , double price , this.model) : super(brand , price);
+  void displayInfo(){
+    super.displayInfo();
+    print('Model: $model');
   }
-
 }
